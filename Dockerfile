df250faa98e623173cb059d70b6950d0ed37c44a
comfuture <comfuture@iplateia.com>
2025-01-23 12:35:36 +0900

    fix: streamline MinIO setup by removing unnecessary mc client configuration

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
-FROM minio/mc:latest AS mc
 FROM minio/minio:latest
-
-COPY --from=mc /bin/mc /bin/mc
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,2 @@
 #!/bin/sh
-
-if [ -n "$BUCKET_NAME" ]; then
-  # Start the MinIO server in the background
-  minio server /data &
-  
-  # Wait until the MinIO server is up and running
-  sleep 5
-  
-  # Configure the mc client
-  mc alias set myminio http://127.0.0.1:$PORT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
-  
-  # Create a bucket
-  mc mb myminio/$BUCKET_NAME
-  
-  # Run the MinIO server in the foreground
-  wait
-else
-  # Start the MinIO server in the foreground
-  minio server /data
-fi
+minio server /data &
